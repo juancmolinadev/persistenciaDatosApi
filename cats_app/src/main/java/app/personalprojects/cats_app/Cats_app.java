d193807bd11737624bf8ccdd513f717d2af8c119
Juan Camilo Molina <juanc.molinadev@gmail.com>
2024-05-07 14:55:23 -0500

    starting project

---
+++ cats_app/src/main/java/app/personalprojects/cats_app/Cats_app.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package app.personalprojects.cats_app;
+
+/**
+ *
+ * @author MolinAnimation
+ */
+public class Cats_app {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ cats_app/src/main/java/app/personalprojects/cats_app/Start.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app.personalprojects.cats_app;
+
+/**
+ *
+ * @author MolinAnimation
+ */
+public class Start {
+    
+}
